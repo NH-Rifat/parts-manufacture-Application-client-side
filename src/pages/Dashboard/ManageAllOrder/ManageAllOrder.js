7e26871a8580c29e6a08aae6382cdb5ac34b84ca
NH-Rifat <rifatr729@gmail.com>
2022-05-26 13:34:03 +0600

    manage all product show and delete successfully

---
+++ src/pages/Dashboard/ManageAllOrder/ManageAllOrder.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
+import Loading from '../../Shared/Loading/Loading';
+import StatusUpdate from './StatusUpdate';
+
+const ManageAllOrder = () => {
+  const [orders, setOrders] = useState([]);
+  const [status, setStatus] = useState('');
+  const [updated, setUpdated] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/manageAllOrders', {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setOrders(data);
+        console.log(data);
+      });
+  }, [updated]);
+
+  const handleChange = (orderId, status) => {
+    const updatestatus = {
+      status: 'shipped',
+    };
+
+    fetch(`http://localhost:5000/manageAllOrders/${orderId}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+      },
+      body: JSON.stringify(updatestatus),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUpdated(!updated);
+        setStatus('shipped');
+        console.log(data);
+      });
+  };
+
+  return (
+    <div>
+      <h2 className='mb-2'>
+        <span className='text-xl'>Manage</span> All Orders {orders?.length}
+      </h2>
+      <hr />
+      <div className='overflow-x-auto'>
+        <table className='table w-full'>
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Customer</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders?.map((order,index) => (
+              <StatusUpdate
+              index={index+1}
+                key={order._id}
+                order={order}
+                handleChange={handleChange}
+                status={status}
+                updated={updated}
+                setUpdated={setUpdated}
+              ></StatusUpdate>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ManageAllOrder;
---
+++ src/pages/Dashboard/ManageAllOrder/StatusUpdate.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const StatusUpdate = ({ order, status,index, handleChange, setUpdated, updated }) => {
+  const handleDelete = (orderId) => {
+    const processed = window.confirm('are you want to delete');
+    if (processed) {
+      fetch(`http://localhost:5000/manageOrders/${orderId}`, {
+        method: 'DELETE',
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount) {
+            setUpdated(!updated);
+            toast.success(`Order is deleted`);
+          }
+        });
+    }
+  };
+
+  return (
+    // {status==='shipped'?"shipped":"pending"}
+    <tr>
+      <th>{index}</th>
+      <td>{order?.product}</td>
+      <td>{order?.price}</td>
+      <td>{order?.userName}</td>
+      <td>
+        {order.paid ? (
+          <span className='text-green-500 text-xl'>{order.status}</span>
+        ) : (
+          <span className='text-red-500 text-xl'>unpaid</span>
+        )}
+      </td>
+      <td>
+        {order?.paid ? (
+          <button
+            onClick={() => handleChange(order._id, status)}
+            className='btn  bg-gradient-to-r from-accent to-info btn-xs'
+          >
+            Update Order
+          </button>
+        ) : (
+          <button
+            onClick={() => handleDelete(order._id)}
+            className='btn btn-error btn-xs'
+          >
+            Cancel Order
+          </button>
+        )}
+      </td>
+    </tr>
+  );
+};
+
+export default StatusUpdate;
