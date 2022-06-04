070325260e7c3941af7e49a15c2688c1e96fd61e
NH-Rifat <rifatr729@gmail.com>
2022-06-04 20:17:31 +0600

    banner image updated

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import banner from '../../../images/banner.jpg';
+import banner from '../../../images/car-banner.jpg';
 
 const Banner = () => {
   return (
@@ -16,7 +16,10 @@ const Banner = () => {
           <div class='max-w-md'>
             <h1 class='mb-5 text-5xl font-bold'>Hello there</h1>
             <p class='mb-5'>
-              Enjoy an entirely new level of driving experience with our in-depth selection of superior car bulbs, brake pads, spark plugs, and other automotive parts and accessories designed to keep your car running as its absolute best
+              Enjoy an entirely new level of driving experience with our
+              in-depth selection of superior car bulbs, brake pads, spark plugs,
+              and other automotive parts and accessories designed to keep your
+              car running as its absolute best
             </p>
             <button class='btn btn-slate-800'>Let's Explore</button>
           </div>
